d91e432f4bbb4dca84aa77471f69bdba52c4fb58
lucashnvarela <2190728@my.ipleiria.pt>
2022-11-11 21:54:39 +0000

    user model

--- common/models/User.php
+++ common/models/User.php
@@ -24,211 +24,312 @@ use yii\web\IdentityInterface;
  * @property string $password write-only password
  */
 class User extends ActiveRecord implements IdentityInterface {
-    const STATUS_DELETED = 0;
-    const STATUS_INACTIVE = 9;
-    const STATUS_ACTIVE = 10;
+	const STATUS_DELETED = 0;
+	const STATUS_INACTIVE = 9;
+	const STATUS_ACTIVE = 10;
 
+	const ROLE_ADMIN = 'admin';
+	const ROLE_GESTOR = 'gestorBilheteira';
+	const ROLE_CLIENTE = 'cliente';
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function tableName() {
-        return '{{%user}}';
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function tableName() {
+		return '{{%user}}';
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public function behaviors() {
-        return [
-            TimestampBehavior::class,
-        ];
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function behaviors() {
+		return [
+			TimestampBehavior::class,
+		];
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public function rules() {
-        return [
-            ['status', 'default', 'value' => self::STATUS_ACTIVE],
-            ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_INACTIVE, self::STATUS_DELETED]],
-        ];
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules() {
+		return [
+			['status', 'default', 'value' => self::STATUS_ACTIVE],
+			['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_INACTIVE, self::STATUS_DELETED]],
+		];
+	}
 
-    /**
-     * @brief Returns the status of the user
-     * 
-     * @return string
-     */
-    public function getStatus() {
-        $status = [
-            0 => 'deleted',
-            9 => 'inactive',
-            10 => 'active',
-        ];
+	/**
+	 * @brief Retorna o status do utilizador
+	 * 
+	 * @return string
+	 */
+	public function getStatus() {
+		$status = [
+			0 => 'eliminado',
+			9 => 'inativo',
+			10 => 'ativo',
+		];
 
-        return $status[$this->status];
-    }
+		return $status[$this->status];
+	}
 
-    /**
-     * @brief Sets the status inactive
-     */
-    public function block() {
-        $this->status = self::STATUS_INACTIVE;
-        return $this->save();
-    }
+	/**
+	 * @brief Define o status do utilizador como inativo
+	 */
+	public function block() {
+		$this->status = self::STATUS_INACTIVE;
+		return $this->save();
+	}
 
-    /**
-     * @brief Sets the status active
-     */
-    public function unblock() {
-        $this->status = self::STATUS_ACTIVE;
-        return $this->save();
-    }
+	/**
+	 * @brief Retorna true se o utilizador estiver inativo
+	 */
+	public function isInactive() {
+		return $this->status == self::STATUS_INACTIVE;
+	}
 
-    /**
-     * @brief Sets the status deleted
-     */
-    public function delete() {
-        $this->status = self::STATUS_DELETED;
-        return $this->save();
-    }
+	/**
+	 * @brief Define o status do utilizador como ativo
+	 */
+	public function unblock() {
+		$this->status = self::STATUS_ACTIVE;
+		return $this->save();
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function findIdentity($id) {
-        return static::findOne(['id' => $id, 'status' => self::STATUS_ACTIVE]);
-    }
+	/**
+	 * @brief Retorna true se o utilizador estiver ativo
+	 */
+	public function isActive() {
+		return $this->status == self::STATUS_ACTIVE;
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function findIdentityByAccessToken($token, $type = null) {
-        throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
-    }
+	/**
+	 * @brief Define o status do utilizador como eliminado
+	 */
+	public function delete() {
+		$this->status = self::STATUS_DELETED;
+		return $this->save();
+	}
 
-    /**
-     * Finds user by username
-     *
-     * @param string $username
-     * @return static|null
-     */
-    public static function findByUsername($username) {
-        return static::findOne(['username' => $username]);
-    }
+	/**
+	 * @brief Retorna true se o utilizador estiver eliminado
+	 */
+	public function isDeleted() {
+		return $this->status == self::STATUS_DELETED;
+	}
 
-    /**
-     * Finds user by password reset token
-     *
-     * @param string $token password reset token
-     * @return static|null
-     */
-    public static function findByPasswordResetToken($token) {
-        if (!static::isPasswordResetTokenValid($token)) {
-            return null;
-        }
+	/**
+	 * @brief Retorna true se o utilizador for cliente
+	 * 
+	 * @return bool
+	 */
+	public function isCliente() {
+		return Yii::$app->authManager->getAssignment(self::ROLE_CLIENTE, $this->id) ? true : false;
+	}
 
-        return static::findOne([
-            'password_reset_token' => $token,
-            'status' => self::STATUS_ACTIVE,
-        ]);
-    }
+	/**
+	 * @brief Retorna true se o utilizador for gestor de bilheteira
+	 * 
+	 * @return bool
+	 */
+	public function isGestor() {
+		return Yii::$app->authManager->getAssignment(self::ROLE_GESTOR, $this->id) ? true : false;
+	}
 
-    /**
-     * Finds user by verification email token
-     *
-     * @param string $token verify email token
-     * @return static|null
-     */
-    public static function findByVerificationToken($token) {
-        return static::findOne([
-            'verification_token' => $token,
-            'status' => self::STATUS_INACTIVE
-        ]);
-    }
+	/**
+	 * @brief Retorna true se o utilizador for admin
+	 * 
+	 * @return bool
+	 */
+	public function isAdmin() {
+		return Yii::$app->authManager->getAssignment(self::ROLE_ADMIN, $this->id) ? true : false;
+	}
 
-    /**
-     * Finds out if password reset token is valid
-     *
-     * @param string $token password reset token
-     * @return bool
-     */
-    public static function isPasswordResetTokenValid($token) {
-        if (empty($token)) {
-            return false;
-        }
+	/**
+	 * @brief Retorna um array com as classes dos icons e ordenação para as colunas username, created_at e status
+	 * 
+	 * @return array
+	 */
+	public static function tableSort($sort_form) {
+		$attr = array_keys($sort_form)[0];
+		$sort = array_values($sort_form)[0];
 
-        $timestamp = (int) substr($token, strrpos($token, '_') + 1);
-        $expire = Yii::$app->params['user.passwordResetTokenExpire'];
-        return $timestamp + $expire >= time();
-    }
+		$columns = ['username', 'created_at', 'status'];
+		$fontawesome = [
+			'DEFAULT' => 'fas fa-arrow-right-arrow-left',
+			'DESC' => [
+				'A-Z' => 'fas fa-arrow-up-z-a',
+				'1-9' => 'fas fa-arrow-down-9-1',
+			],
+			'ASC' => [
+				'A-Z' => 'fas fa-arrow-down-a-z',
+				'1-9' => 'fas fa-arrow-up-1-9',
+			]
+		];
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getId() {
-        return $this->getPrimaryKey();
-    }
+		$sort_config = array();
+		foreach ($columns as $label) {
+			if ($label != $attr) {
+				$sort_config[$label] = [
+					'class' => $fontawesome['DEFAULT'],
+					'sort' => $label != 'username' ? '-' . $label : $label,
+				];
+			} else {
+				$sort_config[$label] = [
+					'class' => $fontawesome[$sort != SORT_ASC ?
+						($attr != 'status' ? 'DESC' : 'ASC') : ($attr != 'status' ? 'ASC' : 'DESC')][$label != 'created_at' ? 'A-Z' : '1-9'],
+					'sort' => $sort != SORT_ASC ? $label : '-' . $label,
+				];
+			}
+		}
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getAuthKey() {
-        return $this->auth_key;
-    }
+		return $sort_config;
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public function validateAuthKey($authKey) {
-        return $this->getAuthKey() === $authKey;
-    }
+	/**
+	 * @brif Gets query for [[Perfil]].
+	 *
+	 * @return \yii\db\ActiveQuery
+	 */
+	public function getPerfil() {
+		return $this->hasOne(Perfil::class, ['id_user' => 'id']);
+	}
 
-    /**
-     * Validates password
-     *
-     * @param string $password password to validate
-     * @return bool if password provided is valid for current user
-     */
-    public function validatePassword($password) {
-        return Yii::$app->security->validatePassword($password, $this->password_hash);
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function findIdentity($id) {
+		return static::findOne(['id' => $id, 'status' => self::STATUS_ACTIVE]);
+	}
 
-    /**
-     * Generates password hash from password and sets it to the model
-     *
-     * @param string $password
-     */
-    public function setPassword($password) {
-        $this->password_hash = Yii::$app->security->generatePasswordHash($password);
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function findIdentityByAccessToken($token, $type = null) {
+		throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
+	}
 
-    /**
-     * Generates "remember me" authentication key
-     */
-    public function generateAuthKey() {
-        $this->auth_key = Yii::$app->security->generateRandomString();
-    }
+	/**
+	 * Finds user by username
+	 *
+	 * @param string $username
+	 * @return static|null
+	 */
+	public static function findByUsername($username) {
+		return static::findOne(['username' => $username]);
+	}
 
-    /**
-     * Generates new password reset token
-     */
-    public function generatePasswordResetToken() {
-        $this->password_reset_token = Yii::$app->security->generateRandomString() . '_' . time();
-    }
+	/**
+	 * Finds user by password reset token
+	 *
+	 * @param string $token password reset token
+	 * @return static|null
+	 */
+	public static function findByPasswordResetToken($token) {
+		if (!static::isPasswordResetTokenValid($token)) {
+			return null;
+		}
 
-    /**
-     * Generates new token for email verification
-     */
-    public function generateEmailVerificationToken() {
-        $this->verification_token = Yii::$app->security->generateRandomString() . '_' . time();
-    }
+		return static::findOne([
+			'password_reset_token' => $token,
+			'status' => self::STATUS_ACTIVE,
+		]);
+	}
 
-    /**
-     * Removes password reset token
-     */
-    public function removePasswordResetToken() {
-        $this->password_reset_token = null;
-    }
+	/**
+	 * Finds user by verification email token
+	 *
+	 * @param string $token verify email token
+	 * @return static|null
+	 */
+	public static function findByVerificationToken($token) {
+		return static::findOne([
+			'verification_token' => $token,
+			'status' => self::STATUS_INACTIVE
+		]);
+	}
+
+	/**
+	 * Finds out if password reset token is valid
+	 *
+	 * @param string $token password reset token
+	 * @return bool
+	 */
+	public static function isPasswordResetTokenValid($token) {
+		if (empty($token)) {
+			return false;
+		}
+
+		$timestamp = (int) substr($token, strrpos($token, '_') + 1);
+		$expire = Yii::$app->params['user.passwordResetTokenExpire'];
+		return $timestamp + $expire >= time();
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getId() {
+		return $this->getPrimaryKey();
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getAuthKey() {
+		return $this->auth_key;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function validateAuthKey($authKey) {
+		return $this->getAuthKey() === $authKey;
+	}
+
+	/**
+	 * Validates password
+	 *
+	 * @param string $password password to validate
+	 * @return bool if password provided is valid for current user
+	 */
+	public function validatePassword($password) {
+		return Yii::$app->security->validatePassword($password, $this->password_hash);
+	}
+
+	/**
+	 * Generates password hash from password and sets it to the model
+	 *
+	 * @param string $password
+	 */
+	public function setPassword($password) {
+		$this->password_hash = Yii::$app->security->generatePasswordHash($password);
+	}
+
+	/**
+	 * Generates "remember me" authentication key
+	 */
+	public function generateAuthKey() {
+		$this->auth_key = Yii::$app->security->generateRandomString();
+	}
+
+	/**
+	 * Generates new password reset token
+	 */
+	public function generatePasswordResetToken() {
+		$this->password_reset_token = Yii::$app->security->generateRandomString() . '_' . time();
+	}
+
+	/**
+	 * Generates new token for email verification
+	 */
+	public function generateEmailVerificationToken() {
+		$this->verification_token = Yii::$app->security->generateRandomString() . '_' . time();
+	}
+
+	/**
+	 * Removes password reset token
+	 */
+	public function removePasswordResetToken() {
+		$this->password_reset_token = null;
+	}
 }
--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -14,121 +14,119 @@ use common\models\User;
  * Site controller
  */
 class SiteController extends Controller {
-    /**
-     * {@inheritdoc}
-     */
-    public function behaviors() {
-        return [
-            'access' => [
-                'class' => AccessControl::class,
-                'only' => ['logout', 'signup'],
-                'rules' => [
-                    [
-                        'actions' => ['signup'],
-                        'allow' => true,
-                        'roles' => ['?'],
-                    ],
-                    [
-                        'actions' => ['logout'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
-            'verbs' => [
-                'class' => VerbFilter::class,
-                'actions' => [
-                    'logout' => ['post'],
-                ],
-            ],
-        ];
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function behaviors() {
+		return [
+			'access' => [
+				'class' => AccessControl::class,
+				'only' => ['logout', 'signup'],
+				'rules' => [
+					[
+						'actions' => ['signup'],
+						'allow' => true,
+						'roles' => ['?'],
+					],
+					[
+						'actions' => ['logout'],
+						'allow' => true,
+						'roles' => ['cliente'],
+					],
+				],
+			],
+			'verbs' => [
+				'class' => VerbFilter::class,
+				'actions' => [
+					'logout' => ['post'],
+				],
+			],
+		];
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public function actions() {
-        return [
-            'error' => [
-                'class' => \yii\web\ErrorAction::class,
-            ],
-            'captcha' => [
-                'class' => \yii\captcha\CaptchaAction::class,
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
-        ];
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function actions() {
+		return [
+			'error' => [
+				'class' => \yii\web\ErrorAction::class,
+			],
+			'captcha' => [
+				'class' => \yii\captcha\CaptchaAction::class,
+				'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+			],
+		];
+	}
 
-    /**
-     * Displays homepage.
-     *
-     * @return mixed
-     */
-    public function actionIndex() {
-        return $this->render('index');
-    }
+	/**
+	 * Displays homepage.
+	 *
+	 * @return mixed
+	 */
+	public function actionIndex() {
+		return $this->render('index');
+	}
 
-    /**
-     * Logs in a user.
-     *
-     * @return mixed
-     */
-    public function actionLogin() {
-        if (!Yii::$app->user->isGuest) {
-            return $this->goHome();
-        }
+	/**
+	 * Logs in a user.
+	 *
+	 * @return mixed
+	 */
+	public function actionLogin() {
+		if (!Yii::$app->user->isGuest) return $this->goHome();
 
-        $model_login = new LoginForm();
-        if ($model_login->load(Yii::$app->request->post())) {
+		$model_login = new LoginForm();
+		if ($model_login->load(Yii::$app->request->post())) {
 
-            $user_login = User::findByUsername($model_login->username);
+			$user_login = User::findByUsername($model_login->username);
 
-            if (is_null($user_login))
-                Yii::$app->session->setFlash('error', 'Username ou password incorretos');
+			if (is_null($user_login))
+				Yii::$app->session->setFlash('error', 'Username ou password incorretos');
 
-            elseif ($user_login->validatePassword($model_login->password)) {
-                if (Yii::$app->authManager->getAssignment('cliente', $user_login->id) != null) {
-                    if ($user_login->status == User::STATUS_ACTIVE) {
-                        $model_login->login();
+			elseif ($user_login->isCliente()) {
+				if ($user_login->isActive()) {
+					if ($user_login->validatePassword($model_login->password)) {
+						$model_login->login();
 
-                        return $this->redirect('index');
-                    } else Yii::$app->session->setFlash('error', 'Login indisponível');
-                } else Yii::$app->session->setFlash('error', 'Sem permissão de acesso');
-            } else Yii::$app->session->setFlash('error', 'Username ou password incorretos');
-        }
+						return $this->goHome();
+					} else Yii::$app->session->setFlash('error', 'Username ou password incorretos');
+				} else Yii::$app->session->setFlash('error', 'Login indisponível');
+			} else Yii::$app->session->setFlash('error', 'Sem permissão de acesso');
+		}
 
-        $model_login->password = '';
+		$model_login->password = '';
 
-        return $this->render('login', [
-            'model_login' => $model_login,
-        ]);
-    }
+		return $this->render('login', [
+			'model_login' => $model_login,
+		]);
+	}
 
-    /**
-     * Logs out the current user.
-     *
-     * @return mixed
-     */
-    public function actionLogout() {
-        Yii::$app->user->logout();
+	/**
+	 * Logs out the current user.
+	 *
+	 * @return mixed
+	 */
+	public function actionLogout() {
+		Yii::$app->user->logout();
 
-        return $this->goHome();
-    }
+		return $this->goHome();
+	}
 
-    /**
-     * Signs user up.
-     *
-     * @return mixed
-     */
-    public function actionSignup() {
-        $model_signup = new SignupForm();
-        if ($model_signup->load(Yii::$app->request->post()) && $model_signup->signup('cliente')) {
-            Yii::$app->session->setFlash('success', 'Registo efetuado com sucesso');
-            return $this->goHome();
-        }
+	/**
+	 * Signs user up.
+	 *
+	 * @return mixed
+	 */
+	public function actionSignup() {
+		$model_signup = new SignupForm();
+		if ($model_signup->load(Yii::$app->request->post()) && $model_signup->signup(User::ROLE_CLIENTE)) {
+			//Yii::$app->session->setFlash('success', 'Registo efetuado com sucesso');
+			return $this->goHome();
+		}
 
-        return $this->render('signup', [
-            'model_signup' => $model_signup,
-        ]);
-    }
+		return $this->render('signup', [
+			'model_signup' => $model_signup,
+		]);
+	}
 }
