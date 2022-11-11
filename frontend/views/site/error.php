26a9d707a10231cec5d48ac3d8d98fe8573ecca4
lucashnvarela <2190728@my.ipleiria.pt>
2022-11-11 21:50:59 +0000

    error page

--- frontend/views/site/error.php
+++ frontend/views/site/error.php
@@ -5,22 +5,18 @@
 /** @var string $message */
 /** @var Exception $exception */
 
-use yii\helpers\Html;
-
-$this->registerCssFile("@web/css/error.css");
+$this->registerCssFile("@web/css/site/error.css");
 
 $this->title = $name;
 ?>
 
-<div class="h-100 d-flex flex-column justify-content-center align-items-center">
-    <div class="alert alert-danger page-not-found">
-        <div class="text-center">
-            <h3>Page not found</h3>
-            <p>The requested URL was not found on this server.<br>
-                Please contact us if you think this is a server error.<br>
-                Thank you.</p>
-
-            <?= Html::a('', ['/site/index'], ['class' => 'btn btn-dark rounded-circle ti-home']) ?>
-        </div>
-    </div>
+<div class="error-page">
+	<div class="alert alert-danger page-not-found">
+		<div class="text-center">
+			<h3>Page not found</h3>
+			<p>The requested URL was not found on this server.<br>
+				Please contact us if you think this is a server error.<br>
+				Thank you.</p>
+		</div>
+	</div>
 </div>
