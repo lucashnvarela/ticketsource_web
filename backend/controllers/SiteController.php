62846786fab66a21c857ac4b5db0910d6d601660
lucashnvarela <2190728@my.ipleiria.pt>
2022-11-11 21:58:19 +0000

    dashboard

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -10,123 +10,146 @@ use yii\web\Response;
 use common\models\LoginForm;
 use common\models\SignupForm;
 use common\models\User;
+use common\models\Evento;
+use common\models\Sessao;
+use common\models\Bilhete;
+use frontend\models\FaturaBilhete;
 
 /**
  * Site controller
  */
 class SiteController extends Controller {
-    /**
-     * {@inheritdoc}
-     */
-    public function behaviors() {
-        return [
-            'access' => [
-                'class' => AccessControl::class,
-                'rules' => [
-                    [
-                        'actions' => ['login', 'error'],
-                        'allow' => true,
-                    ],
-                    [
-                        'actions' => ['logout', 'index', 'signup'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
-            'verbs' => [
-                'class' => VerbFilter::class,
-                'actions' => [
-                    'logout' => ['post'],
-                ],
-            ],
-        ];
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function behaviors() {
+		return [
+			'access' => [
+				'class' => AccessControl::class,
+				'only' => ['logout', 'signup', 'index'],
+				'rules' => [
+					[
+						'actions' => ['signup'],
+						'allow' => true,
+						'roles' => ['admin'],
+					],
+					[
+						'actions' => ['logout', 'index'],
+						'allow' => true,
+						'roles' => ['admin', 'gestorBilheteira'],
+					],
+				],
+			],
+			'verbs' => [
+				'class' => VerbFilter::class,
+				'actions' => [
+					'logout' => ['post'],
+				],
+			],
+		];
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public function actions() {
-        return [
-            'error' => [
-                'class' => \yii\web\ErrorAction::class,
-            ],
-        ];
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function actions() {
+		return [
+			'error' => [
+				'class' => \yii\web\ErrorAction::class,
+			],
+		];
+	}
 
-    /**
-     * Displays homepage.
-     *
-     * @return string
-     */
-    public function actionIndex() {
-        return $this->render('index');
-    }
+	/**
+	 * Displays dashboard.
+	 *
+	 * @return string
+	 */
+	public function actionIndex() {
+		//* Numero de Eventos
+		$db_evento = Evento::find()->all();
 
-    /**
-     * Login action.
-     *
-     * @return string|Response
-     */
-    public function actionLogin() {
-        if (!Yii::$app->user->isGuest) {
-            return $this->goHome();
-        }
+		//* Numero de Sessoes
+		$db_sessao = Sessao::find()->all();
 
-        $this->layout = 'blank';
+		//* Numero de Bilhetes
+		$db_bilhete = Bilhete::find()->all();
 
-        $model_login = new LoginForm();
-        if ($model_login->load(Yii::$app->request->post())) {
+		//* Total de Rendimentos
+		$db_faturabilhete = FaturaBilhete::find()->all();
+		$tRendimentos = 0;
+		foreach ($db_faturabilhete as $faturabilhete) $tRendimentos += $faturabilhete->preco;
 
-            $user_login = User::findByUsername($model_login->username);
+		return $this->render('index', [
+			'nEventos' => count($db_evento),
+			'nSessoes' => count($db_sessao),
+			'nBilhetes' => count($db_bilhete),
+			'tRendimentos' => $tRendimentos,
+		]);
+	}
 
-            if (is_null($user_login))
-                Yii::$app->session->setFlash('error', 'Username ou password incorretos');
+	/**
+	 * Login action.
+	 *
+	 * @return string|Response
+	 */
+	public function actionLogin() {
+		if (!Yii::$app->user->isGuest) return $this->goHome();
 
-            elseif ($user_login->validatePassword($model_login->password)) {
-                if (Yii::$app->authManager->getAssignment('cliente', $user_login->id) == null) {
-                    if ($user_login->status == User::STATUS_ACTIVE) {
-                        $model_login->login();
+		$this->layout = 'blank';
 
-                        return $this->redirect('index');
-                    } else Yii::$app->session->setFlash('error', 'Login indisponível');
-                } else Yii::$app->session->setFlash('error', 'Sem permissão de acesso');
-            } else Yii::$app->session->setFlash('error', 'Username ou password incorretos');
-        }
+		$model_login = new LoginForm();
+		if ($model_login->load(Yii::$app->request->post())) {
 
-        $model_login->password = '';
+			$user_login = User::findByUsername($model_login->username);
 
-        return $this->render('login', [
-            'model_login' => $model_login,
-        ]);
-    }
+			if (is_null($user_login))
+				Yii::$app->session->setFlash('error', 'Username ou password incorretos');
 
-    /**
-     * Logout action.
-     *
-     * @return Response
-     */
-    public function actionLogout() {
-        Yii::$app->user->logout();
+			elseif (!$user_login->isCliente()) {
+				if ($user_login->isActive()) {
+					if ($user_login->validatePassword($model_login->password)) {
+						$model_login->login();
 
-        return $this->goHome();
-    }
+						return $this->goHome();
+					} else Yii::$app->session->setFlash('error', 'Username ou password incorretos');
+				} else Yii::$app->session->setFlash('error', 'Login indisponível');
+			} else Yii::$app->session->setFlash('error', 'Sem permissão de acesso');
+		}
 
-    /**
-     * Signs user up.
-     *
-     * @return mixed
-     */
-    public function actionSignup() {
+		$model_login->password = '';
 
-        $model_signup = new SignupForm();
-        if ($model_signup->load(Yii::$app->request->post()) && $model_signup->signup('gestorBilheteira')) {
-            Yii::$app->session->setFlash('success', 'Registo efetuado com sucesso');
-            return $this->goHome();
-        }
+		return $this->render('login', [
+			'model_login' => $model_login,
+		]);
+	}
 
-        return $this->render('signup', [
-            'model_signup' => $model_signup,
-        ]);
-    }
+	/**
+	 * Logout action.
+	 *
+	 * @return Response
+	 */
+	public function actionLogout() {
+		Yii::$app->user->logout();
+
+		return $this->goHome();
+	}
+
+	/**
+	 * Signs user up.
+	 *
+	 * @return mixed
+	 */
+	public function actionSignup() {
+
+		$model_signup = new SignupForm();
+		if ($model_signup->load(Yii::$app->request->post()) && $model_signup->signup(User::ROLE_GESTOR)) {
+			//Yii::$app->session->setFlash('success', 'Registo efetuado com sucesso');
+			return $this->redirect(['user/index', 'sort' => 'username']);
+		}
+
+		return $this->render('signup', [
+			'model_signup' => $model_signup,
+		]);
+	}
 }
--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -1,17 +1,53 @@
 <?php
 
 /** @var $this yii\web\View */
+/** @var $nEventos common\models\Evento */
+/** @var $nSessoes common\models\Sessao */
+/** @var $nBilhetes common\models\Bilhete */
+/** @var $tRendimentos frontend\models\FaturaBilhete */
 
 $this->registerCssFile("@web/css/site/index.css");
-
-\hail812\adminlte3\assets\AdminLteAsset::register($this);
 
 $this->title = 'Dashboard';
 ?>
 
-<div class="site-index container">
-    <div class="d-flex flex-column justify-content-center align-items-center">
-        <div class="container-fluid">
+<div class="dashboard-page">
+    <div class="dashboard-row">
+        <div class="card info-form">
+            <div class="card-icon">
+                <span><i class="fas fa-calendar-day fa-lg"></i></span>
+            </div>
+            <div class="card-body">
+                <h4>№ de Eventos</h4>
+                <h4><?= $nEventos ?></h4>
+            </div>
+        </div>
+        <div class="card info-form">
+            <div class="card-icon">
+                <span><i class="far fa-calendar-days fa-lg"></i></span>
+            </div>
+            <div class="card-body">
+                <h4>№ de Sessões</h4>
+                <h4><?= $nSessoes ?></h4>
+            </div>
+        </div>
+        <div class="card info-form">
+            <div class="card-icon">
+                <span><i class="fas fa-ticket fa-lg"></i></span>
+            </div>
+            <div class="card-body">
+                <h4>№ de Bilhetes</h4>
+                <h4><?= $nBilhetes ?></h4>
+            </div>
+        </div>
+        <div class="card info-form">
+            <div class="card-icon">
+                <span><i class="far fa-credit-card fa-lg"></i></span>
+            </div>
+            <div class="card-body">
+                <h4>Rendimentos</h4>
+                <h4><?= $tRendimentos ?><span>€</span></h4>
+            </div>
         </div>
     </div>
 </div>
